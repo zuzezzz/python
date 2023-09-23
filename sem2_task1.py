531552a1aac07a163aa724843a45afd4b9cee545
zuzezzz <145129056+zuzezzz@users.noreply.github.com>
2023-09-23 22:44:08 +0300

    Update sem2_task1.py

--- sem2_task1.py
+++ sem2_task1.py
@@ -2,8 +2,8 @@ nums = list(map(int, input().split())
 count = nums[0]
 nums.remove(nums[0])
 for elem in range(1, count + 1):
-        nums.append(elem)
+    nums.append(elem)
 res = 0
 for num in nums:
-        res ^= num
+    res ^= num
 print(res)
