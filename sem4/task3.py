7e218374818d82fed20ea166e7c2838a43fc0fbf
zuzezzz <145129056+zuzezzz@users.noreply.github.com>
2023-09-29 18:21:37 +0300

    Update task3.py

--- sem4/task3.py
+++ sem4/task3.py
@@ -9,13 +9,13 @@ pet_wid = data['PetalWidthCm'].tolist()
 spec = data['Species'].tolist()
 
 
-spis1 = {}
+spis1 = {} 
 for i in spec:
 	spis1[i] = spec.count(i)	
 plt.pie(spis1.values(), labels = spis1.keys())
 plt.title('Species pie')
 #plt.show()
-plt.savefig('species_pie.png')
+plt.savefig('species_pie.png') 
 
 spis2 = {}
 spis2['less than 1.2'] = len([i for i in pet_len if i < 1.2])
@@ -24,7 +24,7 @@ spis2['more than 1.5'] = len([i for i in pet_len if i > 1.5])
 plt.pie(spis2.values(), labels = spis2.keys())
 plt.title('Petal length pie')
 #plt.show()
-plt.savefig('pet_len_pie.png')
+plt.savefig('pet_len_pie.png') 
 
 
 
