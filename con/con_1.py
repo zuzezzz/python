9155df4234f0fb203f21e64ca9b22d6f5aba7a90
zuzezzz <145129056+zuzezzz@users.noreply.github.com>
2023-10-06 17:39:24 +0300

    Update con_1.py

--- con/con_1.py
+++ con/con_1.py
@@ -1,15 +1,8 @@
 spis =  list(input().split())
-print(spis)
-'''for i in range(1, len(spis)):
-	if int(spis[-i] + spis[-i - 1]) > int(spis[-i - 1] + spis[-i]):
-		spis[-i], spis[-i - 1] = spis[-i - 1], spis[-i]
-str = ''.join(spis)
-print(str)'''
 
 for j in range(len(spis) - 1):
 	for i in range(len(spis) - j - 1):
 		if spis[i] + spis[i + 1] <  spis[i + 1] + spis[i]:
 			spis[i], spis[i + 1] = spis[i + 1], spis[i]
-
 
 print(str(''.join(spis)))
