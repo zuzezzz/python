3bde26d63df1625d106c6e720963f8937773b68e
zuzezzz <145129056+zuzezzz@users.noreply.github.com>
2024-02-09 16:36:33 +0300

    Delete con directory

--- con/con_1.py
+++
@@ -1,8 +0,0 @@
-spis =  list(input().split())
-
-for j in range(len(spis) - 1):
-	for i in range(len(spis) - j - 1):
-		if spis[i] + spis[i + 1] <  spis[i + 1] + spis[i]:
-			spis[i], spis[i + 1] = spis[i + 1], spis[i]
-
-print(str(''.join(spis)))
